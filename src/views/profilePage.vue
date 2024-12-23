09442285d4daf4b442bfafde22ded46ef7729aa1
amineallioua <amineallioua123@gmail.com>
2024-12-23 14:00:08 +0100

    profile  responsive

--- src/views/profilePage.vue
+++ src/views/profilePage.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="w-full h-full  px-[65px] pt-[51px]  ">
-        <div class=" flex justify-between mt-5 ">
+    <div class="w-full h-full  md:px-[65px] px-[30px] pt-[51px]  ">
+        <div class=" flex justify-between mt-5 md:flex-row flex-col md-gap-0 gap-3  ">
             <div
-                class=" relative w-[70%] h-[260px] bg-[#CEE1E5] bg-opacity-5 px-[36px] p-[10px] shadow-custom rounded-[20px] ">
-                <p class=" absolute text-[#4796A9] font-[400] text-[10px] bottom-5 right-5 "> X Send message</p>
+                class=" relative md:w-[70%] w-full md:h-[260px] bg-[#CEE1E5] bg-opacity-5 md:px-[36px] px-[10px] p-[10px] shadow-custom rounded-[20px] ">
+                <p class=" absolute text-[#4796A9] font-[400] text-[10px] md:bottom-5 bottom-1 right-5 "> X Send message</p>
                 <h1 class=" font-[500] text-[22px] text-[#4796A9] mb-6 ">Welcome!</h1>
-                <div class=" flex gap-8 ">
-                    <div class=" min-w-[147px] h-[147px] bg-black rounded-[20px] overflow-hidden ">
+                <div class="  flex md:flex-row flex-col  gap-8 ">
+                    <div class=" min-w-[147px] max-w-[147px] h-[147px] bg-black rounded-[20px] overflow-hidden ">
                         <img src="../assets/personal.jpeg" class=" w-full h-full object-cover "alt="">
                     </div>
-                    <div class="w-full flex flex-col gap-1 pr-10 ">
+                    <div class="w-full flex flex-col gap-1 lg:pr-10 ">
                         <div class=" flex justify-between h-[20px]   ">
                             <h1 class=" font-[500] text-[18px] text-[#4796A9] ">User Name</h1>
                             <router-link to="/newHotel">
@@ -19,7 +19,7 @@
                             </router-link>
                         </div>
                         <p class=" text-[#4796A9] text-[12px] font-[400] "> X user contry</p>
-                        <p class=" text-[#729FAA] font-[400] text-[10px] mt-1  ">Lorem Ipsum is simply dummy text of the
+                        <p class=" text-[#729FAA] font-[400] lg:text-[10px] text-[8px] mt-1  ">Lorem Ipsum is simply dummy text of the
                             printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
                             ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
                             a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -28,7 +28,7 @@
                     </div>
                 </div>
             </div>
-            <div class=" relative w-[28%] h-[130px] overflow-hidden   shadow-custom rounded-[20px] ">
+            <div class=" relative md:w-[28%] h-[130px] overflow-hidden   shadow-custom rounded-[20px] ">
                 <div
                     class=" absolute h-full w-full bg-white bg-opacity-95 p-5 flex flex-col justify-between text-[#4796A9] font-[500] text-[12px]  ">
                     <div class=" flex justify-between ">
@@ -59,7 +59,7 @@
 
 
 
-        <div class=" w-full pl-[32px] flex flex-wrap gap-5  " >
+        <div class=" w-full md:pl-[32px] flex flex-wrap gap-5 md:justifuy-normal    justify-center  " >
 
 
 
